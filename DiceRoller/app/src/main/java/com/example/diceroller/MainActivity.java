3455c08ad3cf87c71c8e9237d2abd66bc391b895
Jimmy <jimmydu62100@live.fr>
2020-09-29 14:43:00 +0200

    Exercice2 et 3 TD3

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,10 +19,28 @@ public class MainActivity extends AppCompatActivity {
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int number = (int)(Math.random()*6); //Generate a value between 0 and 6
-                //Toast toast = Toast.makeText(MainActivity.this, ""+number, Toast.LENGTH_SHORT).show(); //Send number in a toast for debuging
-                final TextView resultView = (TextView) findViewById(R.id.result); //Find the TextView in the
-                resultView.setText(""+number);
+                EditText faces = (EditText) findViewById(R.id.faces); //Find the EditText in the layout
+                int numberFaces;
+                try{
+                    numberFaces = Integer.parseInt(faces.getText().toString());
+                }
+                catch (NumberFormatException e){
+                    numberFaces=0;
+                }
+                if(numberFaces > 0 && numberFaces <2000)
+                {
+                    int number = (int)(Math.random()*numberFaces)+1; //Generate a value between 0 and 6
+                    //Toast toast = Toast.makeText(MainActivity.this, ""+number, Toast.LENGTH_SHORT).show(); //Send number in a toast for debuging
+                    TextView resultView = (TextView) findViewById(R.id.result); //Find the TextView in the layout
+                    TextView resultView2 = (TextView) findViewById(R.id.result2); //Find the TextView in the layout
+                    resultView.setText("Dé 1 : "+number); //Set the text value of a TextView
+                    number = (int)(Math.random()*numberFaces)+1;
+                    resultView2.setText("Dé 2 : "+number);//Set the text value of a TextView
+                }else
+                {
+                    Toast.makeText(MainActivity.this, "Choisisez un nombre de faces réaliste !", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
