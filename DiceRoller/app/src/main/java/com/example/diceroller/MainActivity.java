c8513bb234843fe617fd59d15db000f9fb36cbdb
Jimmy <jimmydu62100@live.fr>
2020-09-29 14:01:47 +0200

    Exercice1TD3

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.diceroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button rollButton = (Button) findViewById(R.id.roll_button);
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int number = (int)(Math.random()*6); //Generate a value between 0 and 6
+                //Toast toast = Toast.makeText(MainActivity.this, ""+number, Toast.LENGTH_SHORT).show(); //Send number in a toast for debuging
+                final TextView resultView = (TextView) findViewById(R.id.result); //Find the TextView in the
+                resultView.setText(""+number);
+            }
+        });
     }
+
+
 }
