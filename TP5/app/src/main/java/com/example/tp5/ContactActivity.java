66caf424fd1100120c34a51606f28338149ba1dd
Jimmy <jimmydu62100@live.fr>
2020-10-20 15:12:37 +0200

    TD 5 exo2

--- TP5/app/src/main/java/com/example/tp5/ContactActivity.java
+++ TP5/app/src/main/java/com/example/tp5/ContactActivity.java
@@ -1,14 +1,33 @@
 package com.example.tp5;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
 
-import android.os.Bundle;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.recyclerview.widget.LinearLayoutManager;
+        import androidx.recyclerview.widget.RecyclerView;
+
+        import java.util.ArrayList;
+        import java.util.List;
 
 public class ContactActivity extends AppCompatActivity {
-
+    List<Contact> contacts = new ArrayList<>();
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        contacts.add(new Contact("Hochart","Jimmy","https://media-exp1.licdn.com/dms/image/C4D03AQG8abkO2rOFDA/profile-displayphoto-shrink_200_200/0?e=1608768000&v=beta&t=AJpPyjjXxwxEBfLcJp-LsACpm6zN5xY2PJlG-sCuO3w"));
+        contacts.add(new Contact("Badeuille","Paul","https://media-exp1.licdn.com/dms/image/C4D03AQE6xxOmtdP0pg/profile-displayphoto-shrink_200_200/0?e=1608768000&v=beta&t=GxTwKMUtm_yhqr4tzddaYAWIjNHlmzETQZEyGSSU6rk"));
+        contacts.add(new Contact("Radenne","Gabrielle","https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/72714016_1337064389800981_317061366328328192_o.jpg?_nc_cat=102&_nc_sid=09cbfe&_nc_ohc=OJYA47k-XNcAX88f1N7&_nc_oc=AQlyzsYR97aWTaOuypU5zx2EDFMY2XP0-QQ59C4EYOu_18lZ7Ckwxh6ZAUndEunR7Ig&_nc_ht=scontent-cdg2-1.xx&oh=d877360f322782e316cec4bb588df162&oe=5FB2A1FF"));
+        contacts.add(new Contact("Landrieu","Pioutoine","https://scontent-cdg2-1.xx.fbcdn.net/v/t31.0-8/11427887_128706450794135_4148684143882430205_o.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=JBYsJQ2bC_gAX8CZ12c&_nc_ht=scontent-cdg2-1.xx&oh=2dbf9ae609be47348e454bace316ab9d&oe=5FB43240"));
+        contacts.add(new Contact("Richard","Coeur de Lion","toto"));
+
+        ContactsAdapter adapter = new ContactsAdapter(contacts,this);
+        rvContacts.setAdapter(adapter);
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
 }
--- TP5/app/src/main/java/com/example/tp5/ContactsAdapter.java
+++ TP5/app/src/main/java/com/example/tp5/ContactsAdapter.java
@@ -4,18 +4,23 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 
 public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder>{
     private final List<Contact> mContacts;
+    private final Context moncontext;
 
-    public ContactsAdapter(List<Contact> contacts) {
+    public ContactsAdapter(List<Contact> contacts, Context context) {
         mContacts = contacts;
+        moncontext = context;
     }
     @NonNull
     @Override
@@ -33,7 +38,9 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHo
         TextView firstNameTextView = holder.firstNameTextView;
         firstNameTextView.setText(contact.getmFirstName());
         TextView lastNameTextView = holder.lastNameTextView;
-        firstNameTextView.setText(contact.getmLastName());
+        lastNameTextView.setText(contact.getmLastName());
+        ImageView photoImage = holder.photoImageView;
+        Glide.with(moncontext).load(contact.getImageUrl()).override(400,400).into(photoImage);
     }
 
     @Override
@@ -44,10 +51,12 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHo
     public static class ViewHolder extends RecyclerView.ViewHolder{
         public TextView firstNameTextView;
         public TextView lastNameTextView;
+        public ImageView photoImageView;
         public ViewHolder(View itemView){
             super(itemView);
             firstNameTextView = (TextView) itemView.findViewById(R.id.contact_firstname);
             lastNameTextView = (TextView) itemView.findViewById(R.id.contact_lastname);
+            photoImageView = (ImageView) itemView.findViewById(R.id.image);
 
         }
 
